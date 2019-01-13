dfc259c2ddb31058bc3a08aab1bf6826ca8cbb3d
fr11nik <fruinik@gmail.com>
2019-01-13 16:14:05 +0300

    matrix54

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WindowsFormsApp13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.numericM = new System.Windows.Forms.NumericUpDown();
+            this.numericN = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericM)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericN)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AllowUserToAddRows = false;
+            this.dataGrid.AllowUserToDeleteRows = false;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.ColumnHeadersVisible = false;
+            this.dataGrid.Location = new System.Drawing.Point(91, 80);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.ReadOnly = true;
+            this.dataGrid.RowHeadersVisible = false;
+            this.dataGrid.Size = new System.Drawing.Size(240, 150);
+            this.dataGrid.TabIndex = 0;
+            // 
+            // numericM
+            // 
+            this.numericM.Location = new System.Drawing.Point(91, 54);
+            this.numericM.Name = "numericM";
+            this.numericM.Size = new System.Drawing.Size(120, 20);
+            this.numericM.TabIndex = 1;
+            // 
+            // numericN
+            // 
+            this.numericN.Location = new System.Drawing.Point(217, 54);
+            this.numericN.Name = "numericN";
+            this.numericN.Size = new System.Drawing.Size(120, 20);
+            this.numericN.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Строка";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(214, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Столбец";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(167, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "А,ну давай";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(361, 80);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(149, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Исходная матрица";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(423, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Измененная матрица";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(39, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 165);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(39, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 270);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericN);
+            this.Controls.Add(this.numericM);
+            this.Controls.Add(this.dataGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericN)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.NumericUpDown numericM;
+        private System.Windows.Forms.NumericUpDown numericN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp13
+{
+    public partial class Form1 : Form
+    {//Дана матрица размера M × N. Поменять местами столбец с номером N и первый из столбцов, 
+     //содержащих только отрицательные элементы. Если требуемых столбцов нет, 
+     //то вывести матрицу без изменений.
+        Random R = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            ///Параметры для автоматической смены ширины ячеек
+            dataGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGrid.AutoResizeColumns();
+            dataGrid.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGrid.AutoResizeRows(DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders);
+            //Параметры для автоматической смены ширины ячеек для второй матрицы
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.AutoResizeColumns();
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.AutoResizeRows(DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders);
+
+        }
+        int M, N;
+        void OAIP() // метод заполнения первой матрицы
+        {
+            M = (int)numericM.Value; // присваием переменной M,то что будет на форме нумерика M
+            N = (int)numericN.Value; // присваием переменной N,то что будет на форме нумерика N
+            dataGrid.ColumnCount = N; // N - столбцы
+            dataGrid.RowCount = M; // M - строчки
+            dataGridView1.RowCount = M; // M - Строка
+            dataGridView1.ColumnCount = N; // N - Столбец
+            for (int i = 0; i < M; i++) // цикл для заполнения матрицы
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    dataGrid[j, i].Value = R.Next(-9, 9); // задаем диапозон от -9 до 9 для ячеек матрицы
+                    dataGridView1[j, i].Value = dataGrid[j, i].Value; // задаем такие же значения,как и у первой матрицы
+
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e) // при нажатии на button1
+        {
+            
+            if (numericM.Value == 0 || numericN.Value == 0) // если значения нумериков равна нулю...
+            MessageBox.Show("Введите значения");
+            else
+            OAIP(); // выполнять метод OAIP
+
+            check();
+            if (ourColumn != 98)
+            {
+                Swap();
+            }
+
+        }
+        
+                
+        int[,] masSwap;
+        int ourColumn=98;
+        void check()
+        {
+            
+            bool check = true;
+            for (int j = 0; j < N; j++)
+            {
+                check = true;
+                for ( int i = 0; i < M; i++)
+                {
+                    if ((int)dataGrid[j, i].Value >= 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check==true)
+                {
+                    label3.Text = j.ToString();
+                    ourColumn = j;
+                    MessageBox.Show("");
+                    break;
+                }
+                else label3.Text = "Исходная матрица";
+            }
+        }
+        public void Swap()
+        {
+             masSwap = new int[M, N];
+                       
+                for (int i = 0; i < M; i++)
+                {
+                    masSwap[i, 0] = (int)dataGridView1[N-1, i].Value;                     
+                    dataGridView1[N - 1, i].Value = dataGrid[ourColumn, i].Value;
+                    dataGridView1[ourColumn, i].Value = masSwap[i, 0];
+                }
+            
+        }
+}
+}
